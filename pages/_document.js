c910ab872caccdfc488b138c2be7b2dac323a34f
1998code <54872601+1998code@users.noreply.github.com>
2023-05-20 21:42:15 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Magic Teleport</title>
       <link rel="stylesheet" href="https://cdn.1998.media/css/fontawesome.css" />
       <body>
         <Main />
