5d5b0527e9428ee22cfbc742e9385dac948ebfe6
MING <54872601+1998code@users.noreply.github.com>
2023-05-22 01:33:48 +0800

    Update shorten.js

--- pages/api/shorten.js
+++ pages/api/shorten.js
@@ -7,37 +7,34 @@ export default async function handler(req, res) {
 
   // Split the URLs into an array, line break or ,
   const urlsArray = urls.split(/[\n,]+/)
-
+  
+  const results = []
+  
   // Shorten each URL
-    // await kv.set('shortenKey', 'originalURL')
-    // need to check shortenKey doesn't already exist, else need to create a new one to prevent overwriting
+  for (const url of urlsArray) {
+    if (url === '') continue
 
-    const results = []
+    // radomly generate a key
+    let key = Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5)
 
-    for (const url of urlsArray) {
-      if (url === '') continue
+    // check if key already exists
+    const checkKey = await kv.get(key)
 
-      // radomly generate a key
-      let key = Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5)
-
-      // check if key already exists
-      const checkKey = await kv.get(key)
-
-      // while check key
-      while (checkKey) {
-        key = Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5)
-      }
-
-      // set the key
-      await kv.set(key, url)
-
-      // push the result to the array
-      results.push({
-        key: key,
-        url: url
-      })
+    // while check key
+    while (checkKey) {
+      key = Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5)
     }
 
-    // Return the results
-    res.status(200).json(results)
+    // set the key
+    await kv.set(key, url)
+
+    // push the result to the array
+    results.push({
+      key: key,
+      url: url
+    })
+  }
+
+  // Return the results
+  res.status(200).json(results)
 }
