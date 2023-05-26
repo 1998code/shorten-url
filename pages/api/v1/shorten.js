0cc0bb7161b57e1fd439fa878fb4ee648cc31d6d
1998code <54872601+1998code@users.noreply.github.com>
2023-05-26 19:02:40 +0800

    Update shorten.js

--- pages/api/v1/shorten.js
+++ pages/api/v1/shorten.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
 
     // push the result to the array
     results.push({
-      key: key,
+      key: key.split("$")[0],
       url: url
     })
   }
