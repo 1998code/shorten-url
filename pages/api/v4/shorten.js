076840bd49c595db8081cb37509d36cfb37078c0
1998code <54872601+1998code@users.noreply.github.com>
2023-09-10 13:22:21 +0800

    Update API

---
+++ pages/api/v4/shorten.js
@@ -0,0 +1,52 @@
+import { kv } from "@vercel/kv"
+import { stringify } from "postcss"
+
+export default async function handler(req, res) {
+  console.log(req.body)
+  // const body = req.body
+  // parse body if string
+  const body = typeof req.body === "string" ? JSON.parse(req.body) : req.body
+
+  // GET urls from POST body
+  const urls = body.urls.trim()
+
+  const password = body.password
+
+  // Split the URLs into an array, line break or ,
+  const urlsArray = urls.split(/[\n,]+/)
+  
+  const results = []
+  
+  // Shorten each URL
+  for (const url of urlsArray) {
+    if (url === '') continue
+
+    // radomly generate a key
+    let key = Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5)
+
+    // check if key already exists
+    const checkKey = await kv.get(key)
+
+    // while check key
+    while (checkKey) {
+      key = Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5)
+    }
+
+    // if password is not null, set the password
+    if (password !== "") {
+      key = key + "$" + password
+    }
+
+    // set the key
+    await kv.set(key, url)
+
+    // push the result to the array
+    results.push({
+      key: key.split("$")[0],
+      url: url
+    })
+  }
+
+  // Return the results
+  res.status(200).json(results)
+}
