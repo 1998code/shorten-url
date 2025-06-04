71ced8c4c36bd428428a8ec95a01c57430b9586f
MING <54872601+1998code@users.noreply.github.com>
2025-06-04 17:10:01 +0800

    Update domain.js

--- pages/api/v6/domain.js
+++ pages/api/v6/domain.js
@@ -1,25 +1,29 @@
 export default async function handler(req, res) {
   const { add } = req.query
 
-  const response = await fetch(
-    `https://api.vercel.com/v9/projects/${process.env.PROJECT_ID_VERCEL}/domains?teamId=${process.env.TEAM_ID_VERCEL}`,
-    {
-      body: `{\n  "name": "${add.replaceAll('http://','').replaceAll('https://','')}"\n}`,
-      headers: {
-        Authorization: `Bearer ${process.env.AUTH_BEARER_TOKEN}`,
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
+  try {
+    const response = await fetch(
+      `https://api.vercel.com/v9/projects/${process.env.PROJECT_ID_VERCEL}/domains?teamId=${process.env.TEAM_ID_VERCEL}`,
+      {
+        body: `{\n  "name": "${add.replaceAll('http://','').replaceAll('https://','')}"\n}`,
+        headers: {
+          Authorization: `Bearer ${process.env.AUTH_BEARER_TOKEN}`,
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      }
+    )
+    
+    var data = await response.json()
+
+    if (data.error) {
+      const { projectName, teamName, ...cleanedData } = data.error
+      data = cleanedData
     }
-  )
-
-  const data = await response.json()
-
-  if (data.error?.code == 'forbidden') {
-    res.status(403).end()
-  } else if (data.error?.code == 'domain_taken') {
-    res.status(409).end()
-  } else {
-    res.status(200).end()
+    
+    return res.status(response.status).json(data)
+    
+  } catch (err) {
+    return res.status(500).json({ error: 'Unexpected error occurred!', details: err.message })
   }
 }
